76aa2fb4817b28ecb16b3a693795a27597e7f9e5
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2025-05-08 09:14:25 +0200

    many to many done

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Subject extends Model
 {
-    public function teacher(){
-        return $this->belongsTo(Teacher::class, 'teachers_id');
+    public function teachers(): BelongsToMany{
+        //return $this->belongsTo(Teacher::class, 'teachers_id');
+        return $this->belongsToMany(Teacher::class);
     }
 }
--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Teacher extends Model
 {
-    public function subjects(){
-        return $this->hasMany(Subject::class, 'teachers_id');
+    /*public function subjects()BelongsToMany{
+        //return $this->hasMany(Subject::class, 'teachers_id');
+    }*/
+
+    public function subjects():BelongsToMany{
+        return $this->belongsToMany(Subject::class );
     }
 }
